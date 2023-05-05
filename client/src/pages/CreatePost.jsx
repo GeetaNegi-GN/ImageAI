4f62864d2821c63d687bf4d5cdec39d682de58c1
Geeta Negi <geetanegi550@gmail.com>
2023-05-05 18:53:06 +0530

    started working on server

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,8 +15,13 @@ const CreatePost = () => {
   const generateImage = () => {};
 
   const handleSubmit = () => {};
-  const handleChange = (e) => {};
-  const handleSurpriseMe = () => {};
+  const handleChange = (e) => {
+    setForm({...form,[e.target.name]:e.target.value})
+  };
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({...form,prompt : randomPrompt})
+  };
   return (
     <section className="max-w-7wl mx-auto">
       <div>
