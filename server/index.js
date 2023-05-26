aa7c2ba5011b2617a3244d70f3b2ee55182a9d8c
Geeta Negi <geetanegi550@gmail.com>
2023-05-26 11:36:25 +0530

    completed

--- server/index.js
+++ server/index.js
@@ -19,24 +19,9 @@ app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
-  res.send("Hello form Dall-E");
-});
-
-router.route("/").post(async (req, res) => {
-  try {
-    const { prompt } = req.body;
-    const aiResponse = await openai.createImage({
-      prompt,
-      n: 1,
-      size: "1024x1024",
-      response_format: "b64_json",
-    });
-    const image = aiResponse.data.data[0].b64_json;
-    res.status(200).json({ photo: image });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error?.response.data.error.message);
-  }
+  res.status(200).json({
+    message: "Hello form Dall-E!",
+  });
 });
 
 const startServer = async () => {
